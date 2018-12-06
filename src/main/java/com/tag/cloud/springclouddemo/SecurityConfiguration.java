a13956b089b718ba6a2328a832e702b89466cd0c
tag6988 <tag6988@163.com>
2018-12-06 17:41:22 +0800

    Initial commit

---
+++ src/main/java/com/tag/cloud/springclouddemo/SecurityConfiguration.java
@@ -0,0 +1,37 @@
+package com.tag.cloud.springclouddemo;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author tag
+ * @date 2018/11/22 17:51
+ * @description
+ */
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    /**
+     * 配置安全信息
+     * - 禁用csrf攻击功能
+     * - 开启所有请求需要验证并且使用http basic进行认证
+     *
+     * @param http
+     * @throws Exception
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf()
+                .disable()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+}
